a9b12ef35363f0fb7ba47380558c1d0d31bb8701
muchira mwangi <muchiramwangi1@gmail.com>
2024-02-16 23:50:35 +0300

    Add more pages and requirement,txt

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home",
+    page_icon=":)",
+    layout = "wide"
+)
+
+st.title("helllo there")
+st.button("submit")
+st.text_input("enter your name")
